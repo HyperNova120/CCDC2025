b9f380ece21b5d0c884df03642e479881219131c
Hypernova <amessmer@themessmers.net>
2025-12-17 14:49:02 -0800

    fixes

--- utils/addActiveResponseRule.sh
+++ utils/addActiveResponseRule.sh
@@ -4,22 +4,23 @@ COMMAND_NAME=$1
 EXE_NAME=$2
 RULE=$3
 
-COMMAND="\
-<command>\n\
-  <name>${COMMAND_NAME}</name>\n\
-  <executable>${EXE_NAME}</executable>\n\
-  <timeout_allowed>yes</timeout_allowed>\n\
+COMMAND="<command>
+  <name>${COMMAND_NAME}</name>
+  <executable>${EXE_NAME}</executable>
+  <timeout_allowed>yes</timeout_allowed>
 </command>"
 
-ACTIVE_RESPONSE="\
-<active-response>\n\
-  <command>${COMMAND_NAME}</command>\n\
-  <location>local</location>\n\
-  <rules_id>${RULE}</rules_id>\n\
-  <timeout>300</timeout>\n\
+ACTIVE_RESPONSE="<active-response>
+  <command>${COMMAND_NAME}</command>
+  <location>local</location>
+  <rules_id>${RULE}</rules_id>
+  <timeout>300</timeout>
 </active-response>"
 
-#sed -i '/<!--/{N;N;s/<!--\n  <active-response>/  <active-response>/;s/  <\/active-response>\n  -->/  <\/active-response>/}' /var/ossec/etc/ossec.conf
-
-
-sed -i "/<\/ossec_config>/i ${COMMAND}\n\n${ACTIVE_RESPONSE}\n" /var/ossec/etc/ossec.conf
+# Only insert if not already present
+if ! grep -q "<name>${COMMAND_NAME}</name>" /var/ossec/etc/ossec.conf; then
+  sed -i "/<\/ossec_config>/i ${COMMAND}\n\n${ACTIVE_RESPONSE}\n" /var/ossec/etc/ossec.conf
+  echo "Added command and active-response for ${COMMAND_NAME} (rule ${RULE})"
+else
+  echo "Command ${COMMAND_NAME} already exists in ossec.conf"
+fi
